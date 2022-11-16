ccc24281cc527b01e26c01c990b1a3db19928337
Ivan Dronek <nomi29251@gmail.com>
2022-11-16 11:49:09 +0200

    Add project files.

---
+++ Laboratory_6_C_Sharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_6_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat ginger = new Cat(5, "Red", 2);
+            Dog graff = new Dog(2, "Black", 1);
+
+            Console.WriteLine(ginger.ToString());
+            Console.WriteLine(ginger.MakeSound());
+            Console.WriteLine(graff.ToString());
+            Console.WriteLine(graff.MakeSound());
+
+            Console.ReadKey();
+
+        }
+    }
+}
